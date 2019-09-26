87974adb103663bca81ed3beabc0fe3f1783a7fb
fangxiao <fangxiao@pachiratech.com>
2019-09-26 14:44:09 +0800

    Update.

--- littleScheme.scm
+++ littleScheme.scm
@@ -224,3 +224,155 @@
       (cond
        ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
        (else (all-nums (cdr lat))))))))
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+     ((and (number? a1) (number? a2)) (= a1 a2))
+     ((or (number? a1) (number? a2)) #f)
+     (else (eq? a1 a2)))))
+(define occur
+  (lambda (a lat)
+    (cond
+     ((null? lat) 0)
+     (else
+      (cond
+       ((eq? (car lat) a) (add1 (occur a (cdr lat))))
+       (else
+        (occur a (cdr lat))))))))
+;; (define one?
+;;   (lambda (n)
+;;     (cond
+;;      ((zero? (sub1 n)) #t)
+;;      (else #f))))
+(define one?
+  (lambda (n)
+    (= n 1)))
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((one? n) (cdr lat))
+     (else
+      (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+;;chapter 5
+(define rember*
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((atom? (car lat))
+      (cond
+       ((eq? (car lat) a) (rember* a (cdr lat)))
+       (else
+        (cons (car lat) (rember* a (cdr lat))))))
+     (else
+      (cons (rember* a (car lat)) (rember* a (cdr lat)))))))
+(define insertR*
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((atom? (car lat))
+      (cond
+       ((eq? (car lat) old)
+        (cons old
+              (cons new (insertR* new old (cdr lat)))))
+       (else
+        (cons (car lat) (insertR* new old (cdr lat))))))
+     (else
+      (cons (insertR* new old (car lat)) (insertR* new old (cdr lat)))))))
+(define occur*
+  (lambda (a lat)
+    (cond
+     ((null? lat) 0)
+     ((atom? (car lat))
+      (cond
+       ((eq? (car lat) a)
+        (add1 (occur* a (cdr lat))))
+       (else
+        (occur* a (cdr lat)))))
+     (else
+      (+ (occur* a (car lat)) (occur* a (cdr lat)))))))
+(define subst*
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((atom? (car lat))
+      (cond
+       ((eq? (car lat) old)
+        (cons new (subst* new old (cdr lat))))
+       (else
+        (cons (car lat) (subst* new old (cdr lat))))))
+     (else
+      (cons (subst* new old (car lat)) (subst* new old (cdr lat)))))))
+(define insertL*
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((atom? (car lat))
+      (cond
+       ((eq? (car lat) old) (cons new (cons old (insertL* new old (cdr lat)))))
+       (else
+        (cons (car lat) (insertL* new old (cdr lat))))))
+     (else
+      (cons (insertL* new old (car lat)) (insertL* new old (cdr lat)))))))
+(define member*
+  (lambda (a lat)
+    (cond
+     ((null? lat) #f)
+     ((atom? (car lat))
+      (cond
+       ((eq? (car lat) a) #t)
+       (else
+        (member* a (cdr lat)))))
+     (else
+      (or (member* a (car lat)) (member* a (cdr lat)))))))
+(define leftmost
+  (lambda (lat)
+    (cond
+     ((atom? (car lat))
+      (car lat))
+     (else
+      (leftmost (car lat))))))
+;; (define eqlist?
+;;   (lambda (l1 l2)
+;;     (cond
+;;      ((and (null? l1) (null? l2)) #t)
+;;      ((and (null? l1) (atom? (car l2))) #f)
+;;      ((null? l1) #f)
+;;      ((and (atom? (car l1)) (null? l2)) #f)
+;;      ((and (atom? (car l1)) (atom? (car l2)))
+;;       (and (eqan? (car l1) (car l2)
+;;                   (eqlist? (cdr l1) (cdr l2)))))
+;;      ((atom? (car l1)) #f)
+;;      ((null? l2) #f)
+;;      ((atom? (car l2)) #f)
+;;      (else
+;;       (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))))))
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     ((and (atom? (car l1)) (atom? (car l2)))
+      (and (eqan? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+     ((or (atom? (car l1)) (atom? (car l2))) #f)
+     (else
+      (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))))))
+(define equal?
+  (lambda (s1 s2)
+    (cond
+     ((and (atom? s1) (atom? s2))
+      (eqan? s1 s2))
+     ((or (atom? s1) (atom? s2)) #f)
+     (else (eqlist s1 s2)))))
+(define eqlist?
+  (lambda (l1 l2)
+    ((and (null? l1) (null? l2)) #t)
+    ((or (null? l1) (null? l2)) #f)
+    (else
+     (and (equal? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))))
+;; chapter 6
+(define numbered?
+  (lambda (aexp)
+    (cond
+     ((atom? aexp) (number? aexp))
+     (else
+      (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp)))))))))
